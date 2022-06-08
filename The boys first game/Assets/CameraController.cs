9a3313d9dc18496fa0e73f4c1350322c7ab9a681
Anotherspencer1 <spencertsor@gmail.com>
2022-06-07 20:05:36 -0500

    Added project folder

---
+++ The boys first game/Assets/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform subject;
+    public float moveSpeed = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = Vector3.MoveTowards(subject.position, subject.position, transform.position, moveSpeed * Time.deltaTime);
+    }
+}
