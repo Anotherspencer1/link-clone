120b2128edb41e0fd7a5ac40c1dc540547fe4835
Anotherspencer1 <spencertsor@gmail.com>
2022-06-09 19:02:36 -0500

    Added Animation
    Added animation. Animation and collision now works, but movement system may need to be reworked in future to resolve various bugs

--- The boys first game/Assets/PlayerController.cs
+++ The boys first game/Assets/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Transform movePoint;
+    public Animator animator;
 
     public LayerMask whatStopsMovement;
     public enum PrioritisedInput
@@ -30,6 +31,7 @@ public class PlayerController : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Speed", movement.sqrMagnitude);
 
         if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
@@ -42,6 +44,10 @@ public class PlayerController : MonoBehaviour
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(movement.x, 0f, 0f), .2f, whatStopsMovement))
                 {
                     movePoint.position += new Vector3(movement.x, 0f, 0f);
+                    animator.SetFloat("Horizontal", movement.x);
+                    animator.SetFloat("Vertical", 0);
+                    animator.SetFloat("Direction_x", movement.x);
+                    animator.SetFloat("Direction_y", 0);
                     prioInput = PrioritisedInput.XAxis;
                 }
             }
@@ -50,6 +56,10 @@ public class PlayerController : MonoBehaviour
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, movement.y, 0f), .2f, whatStopsMovement))
                 {
                     movePoint.position += new Vector3(0f, movement.y, 0f);
+                    animator.SetFloat("Vertical", movement.y);
+                    animator.SetFloat("Horizontal", 0);
+                    animator.SetFloat("Direction_y", movement.y);
+                    animator.SetFloat("Direction_x", 0);
                     prioInput = PrioritisedInput.YAxis;
                 }
             }
@@ -58,6 +68,10 @@ public class PlayerController : MonoBehaviour
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, movement.y, 0f), .2f, whatStopsMovement))
                 {
                     movePoint.position += new Vector3(0f, movement.y, 0f);
+                    animator.SetFloat("Vertical", movement.y);
+                    animator.SetFloat("Horizontal", 0);
+                    animator.SetFloat("Direction_y", movement.y);
+                    animator.SetFloat("Direction_x", 0);
                 }
             }
             else if (Mathf.Abs(movement.y) == 1f && Mathf.Abs(movement.x) == 1f && prioInput == PrioritisedInput.YAxis)
@@ -65,11 +79,11 @@ public class PlayerController : MonoBehaviour
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(movement.x, 0f , 0f), .2f, whatStopsMovement))
                 {
                     movePoint.position += new Vector3(movement.x, 0f, 0f);
+                    animator.SetFloat("Horizontal", movement.x);
+                    animator.SetFloat("Vertical", 0);
+                    animator.SetFloat("Direction_x", movement.x);
+                    animator.SetFloat("Direction_y", 0);
                 }
-            }
-            else if (Mathf.Abs(movement.x) == 1f && Mathf.Abs(movement.y) == 1f && prioInput == PrioritisedInput.XAxis)
-            {
-                prioInput = PrioritisedInput.XAxis;
             }
         }
     }
